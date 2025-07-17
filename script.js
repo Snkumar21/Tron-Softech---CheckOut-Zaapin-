98e42e6e4e10ae2a3d302df29ee2f2108c0cfb14
Nitish <snkumar6926@gmail.com>
2025-07-17 20:44:10 +0530

    New Changes Added

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// Payment Functionality--
-
-const codSection = document.getElementById('cod-section');
-const onlineSection = document.getElementById('online-section');
-const paymentOptions = document.getElementsByName('payment');
-
-paymentOptions.forEach((option) => {
-    option.addEventListener('change', () => {
-        if (option.value === 'cod') {
-            codSection.classList.remove('hidden');
-            onlineSection.classList.add('hidden');
-        } else if (option.value === 'online') {
-            onlineSection.classList.remove('hidden');
-            codSection.classList.add('hidden');
-        }
-    });
-});
-
 // User Dropdown Functionality--
 
 const userDropdown = document.getElementById('userDropdown');
